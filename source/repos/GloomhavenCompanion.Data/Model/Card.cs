46b930cb31bf199982c710bba635832571b0dfe8
Beaujean Cyril <cy.beaujean@gmail.com>
2024-10-26 17:03:47 +0200

    déplace du gitignore

---
+++ source/repos/GloomhavenCompanion.Data/Model/Card.cs
@@ -0,0 +1,10 @@
+﻿namespace GloomhavenCompanion.API.Model
+{
+  public class Card
+  {
+    public int Id { get; set; }
+    public string Value { get; set; }
+    public bool IsUsed { get; set; } = false;
+    public bool NeedShuffle { get; set; } = false;
+  }
+}
---
+++ source/repos/GloomhavenCompanion.Data/Model/Team.cs
@@ -0,0 +1,9 @@
+﻿namespace GloomhavenCompanion.API.Model
+{
+  public class Team
+  {
+    public int Id { get; set; }
+    public string CompanyName { get; set; }
+    public List<Player> Players { get; set; }
+  }
+}
