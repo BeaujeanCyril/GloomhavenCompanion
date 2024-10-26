172f2c8ee5ef87a308b33d89d7e76e5456fb5839
Beaujean Cyril <cy.beaujean@gmail.com>
2024-10-26 17:04:33 +0200

    Déplacement .gitignore

---
+++ source/repos/GloomhavenCompanion/GloomhavenCompanion.Data/Model/Element.cs
@@ -0,0 +1,10 @@
+﻿namespace GloomhavenCompanion.API.Model
+{
+	public class Element
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public int State { get; set; } = 0;
+		public string ImagePath { get; set; }
+	}
+}
