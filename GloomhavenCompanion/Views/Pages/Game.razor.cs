a59fa4ec404ef54fb887e1fe94393af65e0969e1
Beaujean Cyril <cy.beaujean@gmail.com>
2024-11-12 21:39:20 +0100

    ajout d'un timer de round

--- GloomhavenCompanion/Views/Pages/Game.razor.cs
+++ GloomhavenCompanion/Views/Pages/Game.razor.cs
@@ -1,7 +1,21 @@
-﻿namespace GloomhavenCompanion.Views.Pages;
+﻿using System.Timers;
+
+namespace GloomhavenCompanion.Views.Pages;
 public partial class Game
 {
-	private bool showPopup = false;
+
+  private bool showPopup = false;
+  private bool showTimerPopup = false; // Contrôle l'affichage du pop-up après 5 minutes
+  private System.Timers.Timer roundTimer;
+
+  protected override void OnInitialized()
+  {
+    // Initialisation du timer pour 5 minutes (300 000 millisecondes)
+    roundTimer = new System.Timers.Timer(300000);
+    roundTimer.Elapsed += OnTimerElapsed;
+    roundTimer.AutoReset = false; // Le timer se déclenche une seule fois après 5 minutes
+  }
+
 
 	private void TogglePopup()
 	{
@@ -11,9 +25,24 @@ public partial class Game
     {
         AppState.NextRound();
         ReduceAllElements();
-    }
+    // Redémarre le timer de 5 minutes
+    showTimerPopup = false;
+    roundTimer.Stop();
+    roundTimer.Start();
+  }
 
-    private void SetElementStrong(int id)
+  private void OnTimerElapsed(object sender, ElapsedEventArgs e)
+  {
+    // Affiche le pop-up une fois que les 5 minutes sont écoulées
+    showTimerPopup = true;
+    InvokeAsync(StateHasChanged); // Met à jour l'UI pour afficher le pop-up
+  }
+
+  private void CloseTimerPopup()
+  {
+    showTimerPopup = false;
+  }
+  private void SetElementStrong(int id)
     {
         var element = AppState.Elements.FirstOrDefault(e => e.Id == id);
         if (element != null && (element.State == 0 || element.State == 1))
